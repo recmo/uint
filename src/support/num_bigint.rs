57ef008604510410cdc3de20db142a5b2bdffeaa
Remco Bloemen <remco@wicked.ventures>
2022-06-03 17:25:14 -0700

    Add BigInt support

--- src/support/num_bigint.rs
+++ src/support/num_bigint.rs
@@ -1,24 +1,24 @@
 //! Support for the [`num-bigint`](https://crates.io/crates/num-bigint) crate.
 #![cfg(feature = "num-bigint")]
 
-use crate::{from::FromUintError, Uint};
-use core::marker::PhantomData;
-use num_bigint::{BigInt, BigUint};
+use crate::{from::ToUintError, Uint};
+use num_bigint::{BigInt, BigUint, Sign};
 
 impl<const BITS: usize, const LIMBS: usize> TryFrom<BigUint> for Uint<BITS, LIMBS> {
-    type Error = FromUintError<BITS, Self>;
+    type Error = ToUintError;
 
+    #[allow(clippy::only_used_in_recursion)] // False positive
     fn try_from(value: BigUint) -> Result<Self, Self::Error> {
         Self::try_from(&value)
     }
 }
 
 impl<const BITS: usize, const LIMBS: usize> TryFrom<&BigUint> for Uint<BITS, LIMBS> {
-    type Error = FromUintError<BITS, Self>;
+    type Error = ToUintError;
 
     fn try_from(value: &BigUint) -> Result<Self, Self::Error> {
         Self::checked_from_limbs_slice(value.to_u64_digits().as_slice())
-            .ok_or(FromUintError::Overflow(PhantomData))
+            .ok_or(ToUintError::ValueTooLarge(BITS))
     }
 }
 
@@ -34,6 +34,38 @@ impl<const BITS: usize, const LIMBS: usize> From<&Uint<BITS, LIMBS>> for BigUint
     }
 }
 
+impl<const BITS: usize, const LIMBS: usize> TryFrom<BigInt> for Uint<BITS, LIMBS> {
+    type Error = ToUintError;
+
+    fn try_from(value: BigInt) -> Result<Self, Self::Error> {
+        Self::try_from(&value)
+    }
+}
+
+impl<const BITS: usize, const LIMBS: usize> TryFrom<&BigInt> for Uint<BITS, LIMBS> {
+    type Error = ToUintError;
+
+    fn try_from(value: &BigInt) -> Result<Self, Self::Error> {
+        let (sign, digits) = value.to_u64_digits();
+        if sign == Sign::Minus {
+            return Err(ToUintError::ValueNegative(BITS));
+        }
+        Self::checked_from_limbs_slice(digits.as_slice()).ok_or(ToUintError::ValueTooLarge(BITS))
+    }
+}
+
+impl<const BITS: usize, const LIMBS: usize> From<Uint<BITS, LIMBS>> for BigInt {
+    fn from(value: Uint<BITS, LIMBS>) -> Self {
+        Self::from(&value)
+    }
+}
+
+impl<const BITS: usize, const LIMBS: usize> From<&Uint<BITS, LIMBS>> for BigInt {
+    fn from(value: &Uint<BITS, LIMBS>) -> Self {
+        Self::from_bytes_le(Sign::Plus, &value.as_le_bytes())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,12 +74,26 @@ mod tests {
 
     #[test]
     #[allow(clippy::unreadable_literal)]
-    fn test_roundtrip() {
+    fn test_roundtrip_biguint() {
         const_for!(BITS in SIZES {
             const LIMBS: usize = nlimbs(BITS);
             type U = Uint<BITS, LIMBS>;
             proptest!(|(value: U)| {
                 let big: BigUint = value.into();
+                let back: U = big.try_into().unwrap();
+                assert_eq!(back, value);
+            });
+        });
+    }
+
+    #[test]
+    #[allow(clippy::unreadable_literal)]
+    fn test_roundtrip_bigint() {
+        const_for!(BITS in SIZES {
+            const LIMBS: usize = nlimbs(BITS);
+            type U = Uint<BITS, LIMBS>;
+            proptest!(|(value: U)| {
+                let big: BigInt = value.into();
                 let back: U = big.try_into().unwrap();
                 assert_eq!(back, value);
             });
