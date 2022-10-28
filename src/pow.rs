c8987f12d329eb966372f515a2e1364f414853e2
Remco Bloemen <remco@wicked.ventures>
2022-10-28 10:37:21 -0700

    Bench overflowing_pow

--- src/pow.rs
+++ src/pow.rs
@@ -203,6 +203,7 @@ pub mod bench {
         const_for!(BITS in BENCH {
             const LIMBS: usize = nlimbs(BITS);
             bench_pow::<BITS, LIMBS>(criterion);
+            bench_overflowing_pow::<BITS, LIMBS>(criterion);
         });
     }
 
@@ -221,16 +222,16 @@ pub mod bench {
         });
     }
 
-    fn bench_wrapping_pow<const BITS: usize, const LIMBS: usize>(criterion: &mut Criterion) {
+    fn bench_overflowing_pow<const BITS: usize, const LIMBS: usize>(criterion: &mut Criterion) {
         let input = (
             Uint::<BITS, LIMBS>::arbitrary(),
             Uint::<BITS, LIMBS>::arbitrary(),
         );
         let mut runner = TestRunner::deterministic();
-        criterion.bench_function(&format!("wrapping_pow/{BITS}"), move |bencher| {
+        criterion.bench_function(&format!("overflowing_pow/{BITS}"), move |bencher| {
             bencher.iter_batched(
                 || input.new_tree(&mut runner).unwrap().current(),
-                |(b, e)| black_box(black_box(b).wrapping_pow(black_box(e))),
+                |(b, e)| black_box(black_box(b).overflowing_pow(black_box(e))),
                 BatchSize::SmallInput,
             );
         });
