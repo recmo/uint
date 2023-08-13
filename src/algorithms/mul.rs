bbe5beff99879ca0e855f2fda56ce7291647ef9f
James <james@prestwi.ch>
2023-08-13 13:03:18 -0700

    lint: clippy in features too

--- src/algorithms/mul.rs
+++ src/algorithms/mul.rs
@@ -227,7 +227,7 @@ fn mac(lhs: &mut u64, a: u64, b: u64, c: u64) -> u64 {
 /// Computes `lhs *= a` and returns the carry.
 pub fn mul_nx1(lhs: &mut [u64], a: u64) -> u64 {
     let mut carry = 0;
-    for lhs in lhs.iter_mut() {
+    for lhs in &mut *lhs {
         let product = u128::muladd(*lhs, a, carry);
         *lhs = product.low();
         carry = product.high();
