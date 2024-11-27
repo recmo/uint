f9802751fad97ddc36e006edfb407f27a8403117
Roman Proskuryakoff <r.proskuryakoff@gmail.com>
2024-11-27 11:20:25 +0400

    Update deps

--- src/support/pyo3.rs
+++ src/support/pyo3.rs
@@ -23,97 +23,57 @@
 #![cfg(feature = "pyo3")]
 #![cfg_attr(docsrs, doc(cfg(feature = "pyo3")))]
 
-use crate::Uint;
-use core::ffi::c_uchar;
+use crate::{nbytes, Uint};
 use pyo3::{
-    exceptions::PyOverflowError, ffi, AsPyPointer, FromPyObject, IntoPy, PyAny, PyErr, PyObject,
-    PyResult, Python, ToPyObject,
+    exceptions::{PyAssertionError, PyOverflowError},
+    types::PyAnyMethods,
+    Bound, FromPyObject, IntoPyObject, IntoPyObjectExt, PyAny, PyErr, PyResult, Python,
 };
 
-impl<const BITS: usize, const LIMBS: usize> ToPyObject for Uint<BITS, LIMBS> {
-    fn to_object(&self, py: Python<'_>) -> PyObject {
-        // Fast path for small ints
+impl<'py, const BITS: usize, const LIMBS: usize> IntoPyObject<'py> for Uint<BITS, LIMBS> {
+    type Target = PyAny;
+    type Output = Bound<'py, Self::Target>;
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        // // Fast path for small ints
         if BITS <= 64 {
             let value = self.as_limbs().first().copied().unwrap_or(0);
-            return unsafe {
-                let obj = ffi::PyLong_FromUnsignedLongLong(value);
-                assert!(!obj.is_null(), "Out of memory");
-                PyObject::from_owned_ptr(py, obj)
-            };
+            return value.into_bound_py_any(py);
         }
 
         // Convert using little endian bytes (trivial on LE archs)
-        // and `_PyLong_FromByteArray`.
         let bytes = self.as_le_bytes();
-        unsafe {
-            let obj =
-                ffi::_PyLong_FromByteArray(bytes.as_ptr().cast::<c_uchar>(), bytes.len(), 1, 0);
-            PyObject::from_owned_ptr(py, obj)
-        }
+
+        bytes.into_pyobject(py)
     }
 }
 
-impl<const BITS: usize, const LIMBS: usize> IntoPy<PyObject> for Uint<BITS, LIMBS> {
-    fn into_py(self, py: Python<'_>) -> PyObject {
-        self.to_object(py)
-    }
-}
-
-impl<'source, const BITS: usize, const LIMBS: usize> FromPyObject<'source> for Uint<BITS, LIMBS> {
-    fn extract(ob: &'source PyAny) -> PyResult<Self> {
-        let mut result = Self::ZERO;
-
-        // On little endian let Python write directly to the uint.
-        #[cfg(target_endian = "little")]
-        let py_result = unsafe {
-            let raw = result.as_le_slice_mut();
-            ffi::_PyLong_AsByteArray(
-                ob.as_ptr().cast::<ffi::PyLongObject>(),
-                raw.as_mut_ptr(),
-                raw.len(),
-                1,
-                0,
-            )
-        };
-
-        // On big endian we use an intermediate
-        #[cfg(not(target_endian = "little"))]
-        let py_result = {
-            let mut raw = vec![0_u8; Self::LIMBS * 8];
-            let py_result = unsafe {
-                ffi::_PyLong_AsByteArray(
-                    ob.as_ptr().cast::<ffi::PyLongObject>(),
-                    raw.as_mut_ptr(),
-                    raw.len(),
-                    1,
-                    0,
-                )
-            };
-            result = Self::try_from_le_slice(raw.as_slice()).ok_or_else(|| {
-                PyOverflowError::new_err(format!("Number to large to fit Uint<{}>", Self::BITS))
-            })?;
-            py_result
-        };
-
-        // Handle error from `_PyLong_AsByteArray`.
-        if py_result != 0 {
-            // A TypeError is set if the value is negative and an Overflow error if the
-            // value does not fit `raw.len()` bytes.
-            return Err(PyErr::fetch(ob.py()));
-        }
-
-        // Check mask since we wrote raw.
-        #[cfg(target_endian = "little")]
-        if let Some(last) = result.as_limbs().last() {
-            if *last > Self::MASK {
+impl<'py, const BITS: usize, const LIMBS: usize> FromPyObject<'py> for Uint<BITS, LIMBS> {
+    fn extract_bound(obj: &Bound<'py, PyAny>) -> PyResult<Self> {
+        // Fast path for small ints
+        if BITS <= 64 {
+            let limb: u64 = obj.extract()?;
+            let (res, overflow) = Self::overflowing_from_limbs_slice(&[limb]);
+            if overflow {
                 return Err(PyOverflowError::new_err(format!(
-                    "Number to large to fit Uint<{}>",
+                    "Number too large to fit Uint<{}>",
                     Self::BITS
                 )));
             }
+            return Ok(res);
         }
 
-        Ok(result)
+        let expected_bytes = nbytes(BITS);
+        let slice: &[u8] = obj.extract()?;
+        if slice.len() != expected_bytes {
+            return Err(PyAssertionError::new_err(format!(
+                "Uint: Number of bytes does not match, expected: {}, got: {}",
+                expected_bytes,
+                slice.len()
+            )));
+        }
+        return Ok(Self::from_le_slice(slice));
     }
 }
 
@@ -134,8 +94,8 @@ mod test {
                 const LIMBS: usize = nlimbs(BITS);
                 type U = Uint<BITS, LIMBS>;
                 proptest!(|(value: U)| {
-                    let obj = value.into_py(py);
-                    let native = obj.extract::<U>(py).unwrap();
+                    let obj = value.into_pyobject(py).unwrap();
+                    let native = obj.extract().unwrap();
                     assert_eq!(value, native);
                 });
             });
@@ -146,18 +106,18 @@ mod test {
     fn test_errors() {
         pyo3::prepare_freethreaded_python();
         Python::with_gil(|py| {
-            let obj = (-1_i64).to_object(py);
-            assert!(obj.extract::<U0>(py).is_err());
-            assert!(obj.extract::<U256>(py).is_err());
+            let obj = (-1_i64).into_pyobject(py).unwrap();
+            assert!(obj.extract::<U0>().is_err());
+            assert!(obj.extract::<U256>().is_err());
 
-            let obj = (1000_i64).to_object(py);
-            assert!(obj.extract::<U0>(py).is_err());
-            assert!(obj.extract::<U8>(py).is_err());
+            let obj = (1000_i64).into_pyobject(py).unwrap();
+            assert!(obj.extract::<U0>().is_err());
+            assert!(obj.extract::<U8>().is_err());
 
-            let obj = U512::MAX.to_object(py);
-            assert!(obj.extract::<U0>(py).is_err());
-            assert!(obj.extract::<U64>(py).is_err());
-            assert!(obj.extract::<U256>(py).is_err());
+            let obj = U512::MAX.into_pyobject(py).unwrap();
+            assert!(obj.extract::<U0>().is_err());
+            assert!(obj.extract::<U64>().is_err());
+            assert!(obj.extract::<U256>().is_err());
         });
     }
 }
