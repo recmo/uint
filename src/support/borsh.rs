db4fc435b13286ce0a9edf1b2d4cb42bb777aff7
James <james@prestwi.ch>
2024-12-23 09:09:16 -0700

    refactor: use unsafe deser for borsh

--- src/support/borsh.rs
+++ src/support/borsh.rs
@@ -6,141 +6,116 @@
 use crate::{Bits, Uint};
 use borsh::{io, BorshDeserialize, BorshSerialize};
 
-macro_rules! impl_borsh_for_uint_and_bits {
-    ($(($bits:expr, $bytes:expr)),*) => {
-        $(
-            impl<const LIMBS: usize> BorshDeserialize for Uint<$bits, LIMBS> {
-                #[inline]
-                fn deserialize_reader<R: io::Read>(reader: &mut R) -> io::Result<Self> {
-                    <[u8; $bytes]>::deserialize_reader(reader).map(Self::from_le_bytes)
-                }
-            }
+impl<const BITS: usize, const LIMBS: usize> BorshDeserialize for Uint<BITS, LIMBS> {
+    #[inline]
+    fn deserialize_reader<R: io::Read>(reader: &mut R) -> io::Result<Self> {
+        let mut limbs = [0; LIMBS];
 
-            impl<const LIMBS: usize> BorshSerialize for Uint<$bits, LIMBS> {
-                #[inline]
-                fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
-                    self.to_le_bytes::<$bytes>().serialize(writer)
-                }
-            }
+        // SAFETY: `limbs` is known to have identical memory layout and
+        // alignment to [u8; LIMBS * 8], which is guaranteed to be larger than
+        // [u8; Self::BYTES].
+        unsafe {
+            let ptr = limbs.as_mut_ptr() as *mut u8;
+            // target is only the first `SELF::BYTES` bytes
+            let target = std::slice::from_raw_parts_mut(ptr, Self::BYTES);
+            // this writes into the memory occupied by `limbs`
+            reader.read_exact(target)?;
+        }
 
-            impl<const LIMBS: usize> BorshDeserialize for Bits<$bits, LIMBS> {
-                #[inline]
-                fn deserialize_reader<R: io::Read>(reader: &mut R) -> io::Result<Self> {
-                    Uint::<$bits, LIMBS>::deserialize_reader(reader).map(Into::into)
-                }
-            }
-
-            impl<const LIMBS: usize> BorshSerialize for Bits<$bits, LIMBS> {
-                #[inline]
-                fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
-                    self.as_uint().serialize(writer)
-                }
-            }
-        )*
-    };
+        // Last part: we need to check that the value fits in the type.
+        // This check is reproduced from the assertion in `from_limbs`.
+        if limbs.last().copied().unwrap_or_default() > Self::MASK {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "value is too large for the type",
+            ));
+        }
+        Ok(Self::from_limbs(limbs))
+    }
 }
 
-impl_borsh_for_uint_and_bits!(
-    // impl every 8 bits between 8 and 256 inclusive
-    (8, 1),
-    (16, 2),
-    (24, 3),
-    (32, 4),
-    (40, 5),
-    (48, 6),
-    (56, 7),
-    (64, 8),
-    (72, 9),
-    (80, 10),
-    (88, 11),
-    (96, 12),
-    (104, 13),
-    (112, 14),
-    (120, 15),
-    (128, 16),
-    (136, 17),
-    (144, 18),
-    (152, 19),
-    (160, 20),
-    (168, 21),
-    (176, 22),
-    (184, 23),
-    (192, 24),
-    (200, 25),
-    (208, 26),
-    (216, 27),
-    (224, 28),
-    (232, 29),
-    (240, 30),
-    (248, 31),
-    (256, 32),
-    // arbitrarily impl for multiples of 256 bits
-    (512, 64),
-    (768, 96),
-    (1024, 128),
-    (1280, 160),
-    (1536, 192),
-    (1792, 224),
-    (2048, 256),
-    (2304, 288),
-    (2560, 320),
-    (2816, 352),
-    (3072, 384),
-    (3328, 416),
-    (3584, 448),
-    (3840, 480),
-    (4096, 512)
-);
+impl<const BITS: usize, const LIMBS: usize> BorshSerialize for Uint<BITS, LIMBS> {
+    #[inline]
+    fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
+        let bytes = self.as_le_bytes_trimmed();
+        writer.write_all(&bytes)
+    }
+}
 
+impl<const BITS: usize, const LIMBS: usize> BorshDeserialize for Bits<BITS, LIMBS> {
+    #[inline]
+    fn deserialize_reader<R: io::Read>(reader: &mut R) -> io::Result<Self> {
+        Uint::<BITS, LIMBS>::deserialize_reader(reader).map(Into::into)
+    }
+}
+
+impl<const BITS: usize, const LIMBS: usize> BorshSerialize for Bits<BITS, LIMBS> {
+    #[inline]
+    fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
+        self.as_uint().serialize(writer)
+    }
+}
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[derive(Debug, BorshDeserialize, BorshSerialize, PartialEq, Eq)]
+    struct Something {
+        is_it: bool,
+        value: Uint<256, 4>,
+    }
+
     #[test]
     fn test_uint() {
-        #[derive(Debug, BorshDeserialize, BorshSerialize, PartialEq, Eq)]
-        struct Something {
-            is_it: bool,
-            value: Uint<240, 4>,
-        }
-
         let something = Something {
             is_it: true,
-            value: Uint::from_limbs([1, 2, 3, 4]),
+            value: Uint::<256, 4>::from_limbs([1, 2, 3, 4]),
         };
-        let mut buf = [0; 31];
+        let mut buf = [0; 33];
 
         something.serialize(&mut buf.as_mut_slice()).unwrap();
         assert_eq!(buf, [
             1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0,
-            0, 0
+            0, 0, 0, 0
         ]);
-        assert_eq!(&something.value.to_le_bytes::<30>(), &buf[1..]);
+        assert_eq!(&something.value.to_le_bytes::<32>(), &buf[1..]);
         assert_eq!(Something::try_from_slice(&mut &buf[..]).unwrap(), something);
+    }
+
+    #[derive(Debug, BorshDeserialize, BorshSerialize, PartialEq, Eq)]
+    struct AnotherThing {
+        is_it: bool,
+        value: Bits<256, 4>,
     }
 
     #[test]
     fn test_bits() {
-        #[derive(Debug, BorshDeserialize, BorshSerialize, PartialEq, Eq)]
-        struct AnotherThing {
-            is_it: bool,
-            value: Bits<224, 4>,
-        }
-
         let another_thing = AnotherThing {
             is_it: true,
-            value: Bits::from_limbs([1, 2, 3, 4]),
+            value: Bits::<256, 4>::from_limbs([1, 2, 3, 4]),
         };
-        let mut buf = [0; 29];
+        let mut buf = [0; 33];
 
         another_thing.serialize(&mut buf.as_mut_slice()).unwrap();
         assert_eq!(buf, [
-            1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0
+            1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0,
+            0, 0, 0, 0
         ]);
-        assert_eq!(&another_thing.value.to_le_bytes::<28>(), &buf[1..]);
+        assert_eq!(&another_thing.value.to_le_bytes::<32>(), &buf[1..]);
         assert_eq!(
             AnotherThing::try_from_slice(&mut &buf[..]).unwrap(),
             another_thing
         );
+    }
+
+    #[test]
+    fn deser_invalid_value() {
+        let buf = [0xff; 4];
+        let mut reader = &mut &buf[..];
+
+        let result = Uint::<31, 1>::deserialize_reader(&mut reader);
+        let err = result.unwrap_err();
+        assert_eq!(err.kind(), io::ErrorKind::InvalidData);
+        assert_eq!(err.to_string(), "value is too large for the type");
     }
 }
