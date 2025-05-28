3742ab0814e5fcf88095931b7cfccde90501bf2c
Dan Cline <6798349+Rjected@users.noreply.github.com>
2025-05-27 21:50:45 -0400

    feat: add wrapping_ benches, shr benches

--- benches/benches/bits.rs
+++ benches/benches/bits.rs
@@ -6,5 +6,14 @@ pub fn group(criterion: &mut Criterion) {
         bench_arbitrary_with(criterion, "overflowing_shl", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
             a.overflowing_shl(b)
         });
+        bench_arbitrary_with(criterion, "overflowing_shr", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+            a.overflowing_shr(b)
+        });
+        bench_arbitrary_with(criterion, "wrapping_shl", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+            a.wrapping_shl(b)
+        });
+        bench_arbitrary_with(criterion, "wrapping_shr", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+            a.wrapping_shr(b)
+        });
     });
 }
