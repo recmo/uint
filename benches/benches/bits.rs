b1935d73945d8f2ec436837062b17e299510481c
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-05-29 15:50:50 +0200

    benches: fix shift bench names

--- benches/benches/bits.rs
+++ benches/benches/bits.rs
@@ -3,16 +3,18 @@ use crate::prelude::*;
 pub fn group(criterion: &mut Criterion) {
     const_for!(BITS in BENCH {
         const LIMBS: usize = nlimbs(BITS);
-        bench_arbitrary_with(criterion, "overflowing_shl", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+        let name = |n: &str| format!("{n}/{BITS}");
+        let input = || (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS);
+        bench_arbitrary_with(criterion, &name("overflowing_shl"), input(), |(a, b)| {
             a.overflowing_shl(b)
         });
-        bench_arbitrary_with(criterion, "overflowing_shr", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+        bench_arbitrary_with(criterion, &name("overflowing_shr"), input(), |(a, b)| {
             a.overflowing_shr(b)
         });
-        bench_arbitrary_with(criterion, "wrapping_shl", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+        bench_arbitrary_with(criterion, &name("wrapping_shl"), input(), |(a, b)| {
             a.wrapping_shl(b)
         });
-        bench_arbitrary_with(criterion, "wrapping_shr", (Uint::<BITS, LIMBS>::arbitrary(), 0..=BITS), move |(a, b)| {
+        bench_arbitrary_with(criterion, &name("wrapping_shr"), input(), |(a, b)| {
             a.wrapping_shr(b)
         });
     });
