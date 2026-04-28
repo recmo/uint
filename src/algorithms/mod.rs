5e8925f4b98c6e7e57c4516b4e2133e4c95f0799
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-04-29 01:14:21 +0200

    perf: compare using subtraction

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -103,22 +103,59 @@ pub fn cmp(a: &[u64], b: &[u64]) -> Ordering {
         Ordering::Equal => {}
         non_eq => return non_eq,
     }
-    for i in (0..a.len()).rev() {
-        match i8::from(a[i] > b[i]) - i8::from(a[i] < b[i]) {
-            -1 => return Ordering::Less,
-            0 => {}
-            1 => return Ordering::Greater,
-            _ => unsafe { core::hint::unreachable_unchecked() },
-        }
-
-        // Equivalent to the following code, but on older rustc versions
-        // performs better:
-        // match a[i].cmp(&b[i]) {
-        //     Ordering::Equal => {}
-        //     non_eq => return non_eq,
-        // }
+    let (r, o) = sub(a, b);
+    if r == 0 {
+        Ordering::Equal
+    } else if o {
+        Ordering::Less
+    } else {
+        Ordering::Greater
     }
-    Ordering::Equal
+}
+
+macro_rules! cmp_fns {
+    ($($name:ident, $op:literal => |$a:ident, $b:ident| $impl:expr),* $(,)?) => {
+        $(
+            /// Compare two limb slices in reverse order, returns `true` if
+            #[doc = concat!("`a ", $op, " b`.")]
+            ///
+            /// Assumes that if the slices are of different length, the longer slice is
+            /// always greater than the shorter slice.
+            #[inline(always)]
+            #[must_use]
+            pub fn $name($a: &[u64], $b: &[u64]) -> bool {
+                $impl
+            }
+        )*
+    };
+}
+
+cmp_fns! {
+    lt, "<"  => |a, b| match a.len().cmp(&b.len()) {
+        Ordering::Equal => sub(a, b).1,
+        non_eq => non_eq.is_lt(),
+    },
+    gt, ">"  => |a, b| lt(b, a),
+    ge, ">=" => |a, b| !lt(a, b),
+    le, "<=" => |a, b| !lt(b, a),
+}
+
+/// `a - b`, returns `((a - b).fold(0, bit_or), overflow)`.
+#[inline]
+fn sub(a: &[u64], b: &[u64]) -> (u64, bool) {
+    assume!(a.len() == b.len());
+    let mut borrow = false;
+    let mut acc = 0;
+    for i in 0..a.len() {
+        let x;
+        (x, borrow) = borrowing_sub(a[i], b[i], borrow);
+        acc |= x;
+    }
+    // HACK: This is a hack to avoid the compiler optimizing too much that the
+    // backend doesn't recognize the `borrowing_sub` chain: https://github.com/rust-lang/rust/issues/143517
+    // SAFETY: Writing to a local variable through a reference is safe.
+    unsafe { core::ptr::write_volatile(&mut acc, acc) };
+    (acc, borrow)
 }
 
 #[inline]
